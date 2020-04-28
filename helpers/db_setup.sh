1e9da0bd1243b53be3f224f09b607b253db38fa8
Keith Thompson <me@keiththomps.com>
2020-04-28 14:19:31 -0400

    Remove old versions of docker before install

--- helpers/db_setup.sh
+++ helpers/db_setup.sh
@@ -3,6 +3,16 @@
 echo "Updating and installing Docker"
 sudo yum update -y
 sudo yum upgrade -y
+
+sudo yum remove docker \
+    docker-client \
+    docker-client-latest \
+    docker-common \
+    docker-latest \
+    docker-latest-logrotate \
+    docker-logrotate \
+    docker-engine
+
 sudo yum install -y yum-utils
 
 sudo yum-config-manager \
